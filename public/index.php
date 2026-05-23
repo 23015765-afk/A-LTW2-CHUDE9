9c9866b8952f9ebe89da1fbb60b702bb3a479b56
HuyenTrang05Nguyen <23015179@st.phenikaa-uni.edu.vn>
2026-05-23 12:17:02 +0700

    Commit 25.05

--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode... 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) { 
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Register the Composer autoloader... Nạp toàn bộ thư viện
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap Laravel and handle the request... Bật nguồn bộ não
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
